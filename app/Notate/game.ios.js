700110dd057424080915e20cd9dd47a5c458df91
Justincadburywong <justincadburywong@gmail.com>
2016-07-23 16:14:34 -0700

    add 'dimensions' to components

--- app/Notate/game.ios.js
+++ app/Notate/game.ios.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import {
 	StyleSheet,
 	View,
+	Dimensions,
 	TouchableHighlight,
 	Text,
 } from 'react-native';
@@ -28,17 +29,16 @@ class Game extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-        <Camera 
-          style={styles.camera}
+				<Camera
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
           captureTarget={Camera.constants.CaptureTarget.disk, 
                          Camera.constants.CaptureQuality.low, 
                          Camera.constants.Orientation.portrait }
           ref={(cam) => {
             this.camera = cam;
           }}>
-          <TouchableHighlight style={styles.timerButton} onPress={this.takePicture.bind(this)}>
-            <Text>Welcome to Notate!</Text>
-          </TouchableHighlight>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
         </Camera>
 			</View>
 		);
@@ -49,5 +49,26 @@ class Game extends Component {
       .catch(err => console.error(err));
   }  
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
 
 module.exports = Game;
