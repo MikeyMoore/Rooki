d0f045faf1d8858d37fc8a8604d44a8f8e318d13
“kasper341” <kasper_341@hotmail.com>
2016-07-24 23:57:29 -0700

    Add call to `getCoordinates` Add HTTP response

--- django_notate/sync_files/views.py
+++ django_notate/sync_files/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# import img_process_runner
 from img_process_runner import getCoordinates
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
+    getCoordinates('../IMG_0141.jpg','../IMG_0142.jpg')
     # import ipdb; ipdb.set_trace()
-    coordinates = getCoordinates('../IMG_0141.jpg','../IMG_0142.jpg')
 
-    return coordinates
+    # return coordinates
+
+    return HttpResponse("Hello, world. You're at the polls index.")
