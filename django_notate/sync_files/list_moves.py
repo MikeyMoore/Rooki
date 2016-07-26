aa09901fe2d7a65eee1b4bc8187155a271358680
MikeyMoore <icarusjedi@gmail.com>
2016-07-26 13:06:32 -0700

    Edit listMoves function

--- django_notate/sync_files/list_moves.py
+++ django_notate/sync_files/list_moves.py
@@ -1,10 +1,26 @@
 def listMoves():
-	for x in range(1, 30):
-		moveNumber = str(x) + ". "
-		move = ""
-		for y in range(0,2):
-			if(y == 0):
-				move = str(y)
-			if(y == 1):
-				move += " .. " + str(y)
-		print moveNumber + move
+
+	# grab each notation object from database => 
+	# one notation object should look something like: 
+	# 'f8Be8'
+	# ============
+	# Notation.all
+
+	finalNotation = ""
+	moveWhite = ""
+	moveBlack = ""
+	for y in range(1,30):
+		for x in range(0, 2):
+			moveNumber = str(y) + ". "
+			indexForNotation = x
+			
+			if(x % 2 == 0):
+				# need to call NotateObject.first
+				moveWhite = "white" + str(x)
+			else:
+				# need to call NotateObject.second
+				moveBlack = " .. " + "black" + str(x)
+		# need to delete first two NotateObjects in database
+		finalNotation += moveNumber + moveWhite + moveBlack + "\n"
+
+	print finalNotation
