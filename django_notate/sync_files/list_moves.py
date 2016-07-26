4d9b26ad666d71097e5d2ac5ebba952f56fe7c53
MikeyMoore <icarusjedi@gmail.com>
2016-07-26 15:08:26 -0700

    Fix index number logic and add notes for delete object from db

--- django_notate/sync_files/list_moves.py
+++ django_notate/sync_files/list_moves.py
@@ -4,22 +4,28 @@ def listMoves():
 	# one notation object should look something like: 
 	# 'f8Be8'
 	# ============
-	# Notation.all
+	# NotationObjects.all
 
+	indexForNotation = 0
 	finalNotation = ""
 	moveWhite = ""
 	moveBlack = ""
 	for y in range(1,30):
 		for x in range(0, 2):
 			moveNumber = str(y) + ". "
-			indexForNotation = x
 			
 			if(x % 2 == 0):
-				# need to call NotateObject.first
-				moveWhite = "white" + str(x)
+				# need to call NotateObject id=indexForNotation
+				moveWhite = "white" + str(indexForNotation)
+				# delete NotateObject
+				# SomeModel.objects.filter(id=id).delete()
+				indexForNotation += 1
 			else:
-				# need to call NotateObject.second
-				moveBlack = " .. " + "black" + str(x)
+				# need to call NotateObject id=indexForNotation
+				moveBlack = " .. " + "black" + str(indexForNotation)
+				# delete NotateObject
+				# SomeModel.objects.filter(id=id).delete()
+				indexForNotation += 1
 		# need to delete first two NotateObjects in database
 		finalNotation += moveNumber + moveWhite + moveBlack + "\n"
 
