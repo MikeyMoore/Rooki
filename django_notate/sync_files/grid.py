514a6e4a10773c5d41be08c4e02dc49aadc8e495
MikeyMoore <icarusjedi@gmail.com>
2016-07-25 18:43:44 -0700

    Move files to sync folder inside of django folder

---
+++ django_notate/sync_files/grid.py
@@ -0,0 +1,225 @@
+def grid_translate(x_coord, y_coord):
+	# obj = obj.flatten()
+
+	x = x_coord
+
+	y = y_coord
+
+	if(x < 1200 and x > 940 and y < 1520 and y > 1340):
+		#"a1"
+		return [0]
+	elif(x < 960 and x > 800 and y < 1520 and y > 1340):
+		#"a2"
+		return [1]
+	elif(x < 800 and x > 700 and y < 1520 and y > 1340):
+		#"a3"
+		return [2]
+	elif(x < 690 and x > 540 and y < 1520 and y > 1340):
+		#"a4"
+		return [3]
+	elif(x < 550 and x > 390 and y < 1520 and y > 1340):
+		#"a5"
+		return [4]
+	elif(x < 410 and x > 260 and y < 1520 and y > 1340):
+		#"a6"
+		return [5]
+	elif(x < 280 and x > 130 and y < 1520 and y > 1340):
+		#"a7"
+		return [6]
+	elif(x < 150 and y < 1520 and y > 1340):
+		#"a8"
+		return [7]
+	elif(x < 1200 and x > 940 and y < 1375 and y > 1225):
+		#"b1"
+		return [8]
+	elif(x < 960 and x > 800 and y < 1375 and y > 1225):
+		#"b2"
+		return [9]
+	elif(x < 800 and x > 700 and y < 1375 and y > 1225):
+		#"b3"
+		return [10]
+	elif(x < 690 and x > 540 and y < 1375 and y > 1225):
+		#"b4"
+		return [11]
+	elif(x < 550 and x > 390 and y < 1375 and y > 1225):
+		#"b5"
+		return [12]
+	elif(x < 410 and x > 260 and y < 1375 and y > 1225):
+		#"b6"
+		return [13]
+	elif(x < 280 and x > 130 and y < 1375 and y > 1225):
+		#"b7"
+		return [14]
+	elif(x < 150 and y < 1375 and y > 1225):
+		#"b8"
+		return [15]
+	elif(x < 1200 and x > 940 and y < 1210 and y > 1100):
+		#"c1"
+		return [16]
+	elif(x < 960 and x > 800 and y < 1210 and y > 1100):
+		#"c2"
+		return [17]
+	elif(x < 800 and x > 700 and y < 1210 and y > 1100):
+		#"c3"
+		return [18]
+	elif(x < 690 and x > 540 and y < 1210 and y > 1100):
+		#"c4"
+		return [19]
+	elif(x < 550 and x > 390 and y < 1210 and y > 1100):
+		#"c5"
+		return [20]
+	elif(x < 410 and x > 260 and y < 1210 and y > 1100):
+		#"c6"
+		return [21]
+	elif(x < 280 and x > 130 and y < 1210 and y > 1100):
+		#"c7"
+		return [22]
+	elif(x < 150 and y < 1210 and y > 1100):
+		#"c8"
+		return [23]
+	elif(x < 1200 and x > 940 and y < 1095 and y > 940):
+		#"d1"
+		return [24]
+	elif(x < 960 and x > 800 and y < 1095 and y > 940):
+		#"d2"
+		return [25]
+	elif(x < 800 and x > 700 and y < 1095 and y > 940):
+		#"d3"
+		return [26]
+	elif(x < 690 and x > 540 and y < 1095 and y > 940):
+		#"d4"
+		return [27]
+	elif(x < 550 and x > 390 and y < 1095 and y > 940):
+		#"d5"
+		return [28]
+	elif(x < 410 and x > 260 and y < 1095 and y > 940):
+		#"d6"
+		return [29]
+	elif(x < 280 and x > 130 and y < 1095 and y > 940):
+		#"d7"
+		return [30]
+	elif(x < 150 and y < 1095 and y > 940):
+		#"d8"
+		return [31]
+	elif(x < 1200 and x > 940 and y < 960 and y > 805):
+		#"e1"
+		return [32]
+	elif(x < 960 and x > 800 and y < 960 and y > 805):
+		#"e2"
+		return [33]
+	elif(x < 800 and x > 700 and y < 960 and y > 805):
+		#"e3"
+		return [34]
+	elif(x < 690 and x > 540 and y < 960 and y > 805):
+		#"e4"
+		return [35]
+	elif(x < 550 and x > 390 and y < 960 and y > 805):
+		#"e5"
+		return [36]
+	elif(x < 410 and x > 260 and y < 960 and y > 805):
+		#"e6"
+		return [37]
+	elif(x < 280 and x > 130 and y < 960 and y > 805):
+		#"e7"
+		return [38]
+	elif(x < 150 and y < 960 and y > 805):
+		#"e8"
+		return [39]
+	elif(x < 1200 and x > 940 and y < 810 and y > 700):
+		#"f1"
+		return [40]
+	elif(x < 960 and x > 800 and y < 810 and y > 700):
+		#"f2"
+		return [41]
+	elif(x < 800 and x > 700 and y < 810 and y > 700):
+		#"f3"
+		return [42]
+	elif(x < 690 and x > 540 and y < 810 and y > 700):
+		#"f4"
+		return [43]
+	elif(x < 550 and x > 390 and y < 810 and y > 700):
+		#"f5"
+		return [44]
+	elif(x < 410 and x > 260 and y < 810 and y > 700):
+		#"f6"
+		return [45]
+	elif(x < 280 and x > 130 and y < 810 and y > 700):
+		#"f7"
+		return [46]
+	elif(x < 150 and y < 810 and y > 700):
+		#"f8"
+		return [47]
+	elif(x < 1200 and x > 940 and y < 685 and y > 550):
+		#"g1"
+		return [48]
+	elif(x < 960 and x > 800 and y < 685 and y > 550):
+		#"g2"
+		return [49]
+	elif(x < 800 and x > 700 and y < 685 and y > 550):
+		#"g3"
+		return [50]
+	elif(x < 690 and x > 540 and y < 685 and y > 550):
+		#"g4"
+		return [51]
+	elif(x < 550 and x > 390 and y < 685 and y > 550):
+		#"g5"
+		return [52]
+	elif(x < 410 and x > 260 and y < 685 and y > 550):
+		#"g6"
+		return [53]
+	elif(x < 280 and x > 130 and y < 685 and y > 550):
+		#"g7"
+		return [54]
+	elif(x < 150 and y < 685 and y > 550):
+		#"g8"
+		return [55]
+	elif(x < 1200 and x > 940 and y < 560 and y > 400):
+		#"h1"
+		return [56]
+	elif(x < 960 and x > 800 and y < 560 and y > 400):
+		#"h2"
+		return [57]
+	elif(x < 800 and x > 700 and y < 560 and y > 400):
+		#"h3"
+		return [58]
+	elif(x < 690 and x > 540 and y < 560 and y > 400):
+		#"h4"
+		return [59]
+	elif(x < 550 and x > 390 and y < 560 and y > 400):
+		#"h5"
+		return [60]
+	elif(x < 410 and x > 260 and y < 560 and y > 400):
+		#"h6"
+		return [61]
+	elif(x < 280 and x > 130 and y < 560 and y > 400):
+		#"h7"
+		return [62]
+	elif(x < 150 and y < 560 and y > 400):
+		#"h8"
+		return [63]
+	else:
+		return "Something is wrong"
+
+
+#8::: if x is less than 150
+#7::: if x is less than 280 and greater than 130
+#6::: if x is less than 410 and greater than 260
+#5::: if x is less than 550 and greater than 390
+#4::: if x is less than 690 and greater than 540
+#3::: if x is less than 800 and greater than 700
+#2::: if x is less than 960 and greater than 800
+#1::: if x is less than 1200 and greater than 940
+
+#a::: if y is less than 1520 and greater than 1340
+#b::: if y is less than 1375 and greater than 1225
+#c::: if y is less than 1210 and greater than 1100
+#d::: if y is less than 1095 and greater than 940
+#e::: if y is less than 960 and greater than 805
+#f::: if y is less than 810 and greater than 700
+#g::: if y is less than 685 and greater than 550
+#h::: if y is less than 560 and greater than 400
+
+
+
+
+print grid_translate(1053, 1486)
---
+++ django_notate/sync_files/return_notation.py
@@ -0,0 +1,56 @@
+# check if you can set a variable equal to a set 
+# then once the differences and similarities are found, move the pieces in the variable at the index of the differences/similarities
+# return to the user a move based upon the similarities and differences
+
+# create two functions 
+  # first function takes in two coordinates (the first is a8, the second is a3) the function will move a rook from a8 to a3
+  # second function takes in two coordinates (the first is a8, the second is a1) the function will move a rook from a8 to a1 and take the rook on a1 off the board
+
+chess1 = ["a8 R", "b8 N", "c8 B", "d8 Q", "e8 K", "f8 B", "g8 N", "h8 R",
+	"a7 P", "b7 P", "c7 P", "d7 P", "e7 P", "f7 P", "g7 P", "h7 P",
+	"a6 P", "b6 P", "c6 P", "d6 P", "e6 P", "f6 P", "g6 P", "h6 P",
+	"a5 o", "b5 o", "c5 o", "d5 o", "e5 o", "f5 o", "g5 o", "h5 o",
+	"a4 o", "b4 o", "c4 o", "d4 o", "e4 o", "f4 o", "g4 o", "h4 o",
+	"a3 o", "b3 o", "c3 o", "d3 o", "e3 o", "f3 o", "g3 o", "h3 o",
+	"a2 P", "b2 P", "c2 P", "d2 P", "e2 P", "f2 P", "g2 P", "h2 P",
+	"a1 R", "b1 N", "c1 B", "d1 Q", "e1 K", "f1 B", "g1 N", "h1 R"]
+	
+chess2 = ["a8 R", "b8 N", "c8 B", "d8 Q", "e8 K", "f8 B", "g8 N", "h8 R",
+	"a7 P", "b7 P", "c7 P", "d7 P", "e7 P", "f7 P", "g7 P", "h7 P",
+	"a6 P", "b6 P", "c6 P", "d6 P", "e6 P", "f6 P", "g6 P", "h6 P",
+	"a5 P", "b5 P", "c5 P", "d5 P", "e5 P", "f5 P", "g5 P", "h5 P",
+	"a4 P", "b4 P", "c4 P", "d4 P", "e4 P", "f4 P", "g4 P", "h4 P",
+	"a3 P", "b3 P", "c3 P", "d3 P", "e3 P", "f3 P", "g3 P", "h3 P",
+	"a2 P", "b2 P", "c2 P", "d2 P", "e2 P", "f2 P", "g2 P", "h2 P",
+	"a1 R", "b1 N", "c1 B", "d1 Q", "e1 K", "f1 B", "g1 N", "h1 R"]
+
+zb = set(chess1)
+	
+zc = set(chess2)
+	
+# matched = zb.intersection(zc) 
+# unmatched = zb.symmetric_difference(zc) 
+
+# print(matched)
+# print(unmatched)
+
+def move_piece(start_index, end_index):
+	# I added this part -Mikey
+	start_index = start_index[0]
+	end_index = end_index[0]
+
+	start_square = chess1[start_index]
+	piece = chess1[start_index][-1]
+	print(piece)
+	print(start_square)
+	chess1[start_index] = chess1[start_index].replace(chess1[start_index][-1], "o")
+	print(chess1[start_index])
+	chess1[end_index] = chess1[end_index].replace(chess1[end_index][-1], piece)
+	print(chess1[end_index])
+	print(chess1)
+	notation = start_square + chess1[end_index][0:2]
+	notation = notation.replace(" ", "")
+	return notation
+    
+
+# move_piece(0, 40)
