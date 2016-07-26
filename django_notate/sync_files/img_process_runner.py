3d4bcb61ebacf9799ab226abeb12f117a62d84c4
MikeyMoore <icarusjedi@gmail.com>
2016-07-25 17:41:45 -0700

    Return two coordinates, clean up redundant files

--- django_notate/sync_files/img_process_runner.py
+++ django_notate/sync_files/img_process_runner.py
@@ -17,6 +17,7 @@ def getAfterCoordinates(img1,img2, new_order=-1):
 	coords = coordinates(new_order)
 	# x axis + y axis
 	twoCoords = [coords[0][0], coords[0][1]] 
+	return twoCoords
 
 def getBeforeCoordinates(img1,img2, new_order=-1):
 	# this saves as BeforeImageMove.jpg
@@ -30,20 +31,17 @@ def getBeforeCoordinates(img1,img2, new_order=-1):
 	coords = coordinates(new_order)
 	# x axis + y axis
 	twoCoords = [coords[0][0], coords[0][1]] 
+	return twoCoords
 	
-
-
-
+	
 if __name__ == "__main__":
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-n", "--new", type=int, default=-1,
 		help="whether or not the new order points should should be used")
 	args = vars(ap.parse_args())
-	# print args
-	# These two images break the before coordinates call
-	# print "F8"
-	# getBeforeCoordinates('../../IMG_0322.jpg','../../IMG_0323.jpg', args['new'])
-	getAfterCoordinates('../../IMG_0333.jpg','../../IMG_0334.jpg', args['new'])
+	
+	print getBeforeCoordinates('../../IMG_0322.jpg','../../IMG_0323.jpg', args['new'])
+	print getAfterCoordinates('../../IMG_0333.jpg','../../IMG_0334.jpg', args['new'])
 
 	# Now we need to remove the images created by OpenCV
 	# os.remove('AfterImageMove.jpg')
