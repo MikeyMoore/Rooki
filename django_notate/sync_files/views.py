7209e0b46255a3b97eb7a89ba9f32c314b846a91
MikeyMoore <icarusjedi@gmail.com>
2016-07-26 17:47:15 -0700

    Fix logic for expected table names in list_moves

--- django_notate/sync_files/views.py
+++ django_notate/sync_files/views.py
@@ -7,18 +7,17 @@ from django.http import HttpResponse
 # import img_process_runner
 # from img_process_runner import getAfterCoordinates
 from list_moves import listMoves
+import sqlite3
+
+conn = sqlite3.connect("db.sqlite3", check_same_thread = False)
+c = conn.cursor()
 
 # Create your views here.
 def index(request):
 
-
-
-
     notation = listMoves()
 
 
-    
-   
     return HttpResponse(notation)
 
 
