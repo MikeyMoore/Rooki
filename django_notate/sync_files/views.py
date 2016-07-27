81c7bcf00eb785cf0eaea29abde864cb39d0ba07
MikeyMoore <icarusjedi@gmail.com>
2016-07-27 00:22:57 -0700

    Cleaned comments, added prints, found :bug:, the jpgs are not loading into OpenCV

--- django_notate/sync_files/views.py
+++ django_notate/sync_files/views.py
@@ -10,94 +10,114 @@ from list_moves import listMoves
 from sync_files.models import Document, Notations
 import sqlite3
 
-# conn = sqlite3.connect("db.sqlite3", check_same_thread = False)
-# c = conn.cursor()
-
 def index(request):
+	# How many images did user upload?
 	imageCount = Document.objects.count()
-	# imageCount = c.execute('SELECT COUNT(*) FROM sync_files_document')
 
-	# # this logic saves the single move in db, 
+	# Since deletion of previous images changes the first ID, 
+	# This finds the first image in the database
 	indexFirst = Document.objects.first().id
+	# This next variable is one we increase within for loop
 	increaseIndex = indexFirst
+	print "increaseIndex"
 	print increaseIndex
 
 	for x in range(indexFirst,(indexFirst + imageCount)):
+		# We cannot run this with only one image file left
 		if (Document.objects.count() != 1):
-			# images should be ImageObject.first and ImageObject.second
-			# notation = run_opencv('../../IMG_0322.jpg','../../IMG_0323.jpg')
+			# This finds the first image to compare
+			print "increaseIndex"
 			print increaseIndex
 			imageFirst = Document.objects.get(id=increaseIndex)
+			print "image one"
+			print imageFirst.docfile
 			increaseIndex += 1
 			
+			# This finds the second image to compare
+			print "increaseIndex"
 			print increaseIndex
 			imageSecond = Document.objects.get(id=(increaseIndex))
+			print "image two"
+			print imageSecond.docfile
 
-			# imageFirst = c.execute('SELECT * FROM sync_files_document WHERE ID=(?)', [x])
-			# imageSecond = c.execute('SELECT * FROM sync_files_document WHERE id=(?)', [x+1])
+			# Now we run the two images through OpenCV and return a notation
 			notation = run_opencv(imageFirst.docfile.url, imageSecond.docfile.url)
+			# We save that notation into the database
 			newNotation = Notations(name=notation)
 			newNotation.save()
-	# 		c.execute('INSERT INTO sync_files_notations VALUES (?,?)', [x, notation])
-		# need logic to delete first image
-	# 		c.execute('DELETE FROM sync_files_document WHERE ID=(?)', x)
+
+			# We delete only the first image
 			imageFirst.delete()
 
-	# # grab each notation object from database => 
-	# # one notation object should look something like: 
-	# # 'f8Be8'
-	# # ============
-	# # NotationObjects.all
+	# How many notations objects do we need to go through?
+	notationCount = Notations.objects.count()
 
+	# Since deletion of previous notations changes the first ID, 
+	# This finds the first notation in the database
 	notationIndexFirst = Notations.objects.first().id
+	# This next variable is one we increase within for loop
 	indexForNotation = notationIndexFirst
-	finalNotation = ""
+
+	# These variable save each instance of notation outside of 
+	# the loop so we can constanly overwrite them
 	moveWhite = ""
 	moveBlack = ""
+
+	# This variable stores the entire list of notations so 
+	# they aren't overwritten by the for loop
+	finalNotation = ""
+
+	# This gets modified each time we go through the for loop
+	# from white's move to black's move, starting with white's move
 	whiteOrBlack = "white"
-	# notationCount = c.execute('SELECT COUNT(*) FROM sync_files_notations')
-	notationCount = Notations.objects.count()
+
 	for y in range(notationIndexFirst,(notationIndexFirst + notationCount)):
+
+		# Stops when there are no more notations
 		if (Notations.objects.count() != 0):
-			# for x in range(0, 2):
+			# This prints out which move it is
+			print "x"
+			print x
+			print "notationIndexFirst"
+			print notationIndexFirst
 			moveNumber = str(notationIndexFirst - (notationIndexFirst-1)) + ". "
 			
+			# notates for White's move
 			if(whiteOrBlack == "white" and Notations.objects.count() != 0):
-	# 			# need to call NotateObject id=indexForNotation
-	# 			moveWhite = c.execute('SELECT * FROM sync_files_notations WHERE ID=(?)', [str(indexForNotation)])
 				moveWhite = Notations.objects.get(id=indexForNotation)
-	# 			# delete NotateObject
-	# 			c.execute('DELETE FROM sync_files_notations WHERE ID=(?)', [str(indexForNotation)])
+
+				# changes variable so that the next notation is black's
 				whiteOrBlack = "black"
 				indexForNotation +=1
 				print "indexForNotation"
 				print indexForNotation
 				print "moveWhite"
 				print moveWhite
+
+				# Saves white's move
 				finalNotation += str(moveNumber) + str(moveWhite)
+				# Deletes the notation from the database
 				moveWhite.delete()
+
+			# notates for Blacks's move
 			elif(whiteOrBlack == "black" and Notations.objects.count() != 0):
-	# 			# need to call NotateObject id=indexForNotation
-	# 			moveBlack = " .. " + "black" + str(indexForNotation)
 				moveBlack =  Notations.objects.get(id=indexForNotation)
-	# 			# delete NotateObject
-	# 			c.execute('DELETE FROM sync_files_notations WHERE ID=(?)', [str(indexForNotation)])
+
+				# changes variable so that the next notation is whites's
 				whiteOrBlack = "white"
 				indexForNotation +=1
 				print "indexForNotation"
 				print indexForNotation
 				print "moveBlack"
 				print moveBlack
+
+				# Saves black's move
 				finalNotation += " .. " + str(moveBlack) + "\n"
+				# Deletes the notation from the database 
 				moveBlack.delete()
 
-
-
-
-	#     # c.execute('SELECT COUNT(*) FROM sync_files_notations')
-	#     # c.execute('SELECT COUNT(*) FROM sync_files_document')
-	#     # imageCount = c.fetchone()
-	#     # return HttpResponse(notation)
+	# This prints the finalNotation (list of all notations) 
+	# into the web browser 
 	return HttpResponse(finalNotation)
 
 def list(request):
